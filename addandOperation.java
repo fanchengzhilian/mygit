8b110683d63d11f2d6ae342f432555b807054fc4
fanchengzhilian <45534270+fanchengzhilian@users.noreply.github.com>
2019-07-15 11:14:38 +0800

    完善运算类
    完善

--- addandOperation.java
+++ addandOperation.java
@@ -37,3 +37,28 @@ Class OperationDiv : Operation
         return result;
     }
 }
+
+
+public class Operation
+{
+    private double _numberA = 0；
+    private double _numberB = 0;
+    
+    public double NumberA
+    {
+        get{ return _numberA;}
+        set{ _numberA = value;}   
+    }
+    
+    public double NumberB
+    {
+        get{ return _numberB;}
+        set{ _numberB = value;}   
+    }
+    
+    public virtual double GetResult()
+    {
+        double result = 0;
+        return result;
+    }
+}
